67211702bc09d3c28029dec69c34741ebbcf0cf0
Kirill Mokevnin <mokevnin@gmail.com>
2020-03-15 18:55:23 -0400

    add tests

--- tests/Feature/ArticleControllerTest.php
+++ tests/Feature/ArticleControllerTest.php
@@ -26,10 +26,38 @@ class ArticleControllerTest extends TestCase
         $response->assertStatus(200);
     }
 
-    /* public function testEdit() */
-    /* { */
-    /*     $article = factory(Article::class)->make(); */
-    /*     $response = $this->get(route('articles.edit', [$article])); */
-    /*     $response->assertStatus(200); */
-    /* } */
+    public function testEdit()
+    {
+        $article = factory(Article::class)->create();
+        $response = $this->get(route('articles.edit', [$article]));
+        $response->assertStatus(200);
+    }
+
+    public function testStore()
+    {
+        $data = factory(Article::class)->make()->toArray();
+        $response = $this->post(route('articles.store'), $data);
+        $response->assertStatus(302);
+
+        $this->assertDatabaseHas('articles', ['name' => $data['name']]);
+    }
+
+    public function testUpdate()
+    {
+        $article = factory(Article::class)->create();
+        $data = factory(Article::class)->make()->toArray();
+        $response = $this->patch(route('articles.update', $article), $data);
+        $response->assertStatus(302);
+
+        $this->assertDatabaseHas('articles', ['name' => $data['name']]);
+    }
+
+    public function testDestroy()
+    {
+        $article = factory(Article::class)->create();
+        $response = $this->delete(route('articles.destroy', [$article]));
+        $response->assertStatus(302);
+
+        $this->assertDatabaseMissing('articles', ['id' => $article->id]);
+    }
 }
